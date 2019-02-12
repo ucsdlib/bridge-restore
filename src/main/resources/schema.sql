0173d6b5197cf68980683002e9a7db4911876812
Michael Ritter <shake@umiacs.umd.edu>
2019-02-12 16:31:40 -0500

    Remove unnecessary tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -49,42 +49,4 @@ CREATE TABLE IF NOT EXISTS `restoration` (
   CONSTRAINT `FK_ejb7a5btov5hyhb0pyvo3yeb7` FOREIGN KEY (`snapshot_id`) REFERENCES `snapshot` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Table structure for table `snapshot_content_item`
---
-CREATE TABLE IF NOT EXISTS `snapshot_content_item` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `modified` datetime NOT NULL,
-  `content_id` longtext COLLATE utf8_bin NOT NULL,
-  `content_id_hash` varchar(50) COLLATE utf8_bin NOT NULL,
-  `metadata` longtext COLLATE utf8_bin,
-  `snapshot_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_30tots9ry24rjg42xn08egdrl` (`snapshot_id`,`content_id_hash`),
-  CONSTRAINT `FK_bif6fhum5u975ks9uo9xufbjh` FOREIGN KEY (`snapshot_id`) REFERENCES `snapshot` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Table structure for table `snapshot_alternate_ids`
---
-CREATE TABLE IF NOT EXISTS `snapshot_alternate_ids` (
-  `snapshot_id` bigint(20) NOT NULL,
-  `snapshot_alternate_id` varchar(255) DEFAULT NULL,
-  KEY `FK_q9se3kgdc5eebpwxqq4843bu3` (`snapshot_id`),
-  CONSTRAINT `FK_q9se3kgdc5eebpwxqq4843bu3` FOREIGN KEY (`snapshot_id`) REFERENCES `snapshot` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Table structure for table `snapshot_history`
---
-CREATE TABLE IF NOT EXISTS `snapshot_history` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `modified` datetime DEFAULT NULL,
-  `history` longtext DEFAULT NULL,
-  `history_date` datetime DEFAULT NULL,
-  `snapshot_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FK_ff91lsj23rrrs3nuovf3hofwl` FOREIGN KEY (`snapshot_id`) REFERENCES `snapshot` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
 SET FOREIGN_KEY_CHECKS=1;
