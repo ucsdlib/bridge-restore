6941fc8348fba7a716db8d39ccf7fc6b15c03e50
Michael Ritter <shake@umiacs.umd.edu>
2019-02-14 17:13:47 -0500

    Probably not functional email reports

---
+++ src/main/java/org/chronopolis/bridge/ReportingService.kt
@@ -0,0 +1,54 @@
+package org.chronopolis.bridge
+
+import org.chronopolis.bridge.config.SmtpConfig
+import org.chronopolis.bridge.models.Result
+import java.time.ZonedDateTime
+import java.util.Properties
+import javax.mail.Message
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.MimeMessage
+
+/**
+ * Just send smtp messages ok
+ *
+ * Still need: smtp settings
+ *
+ * @author shake
+ * @since 1.0
+ */
+class ReportingService(val smtpConfig: SmtpConfig) {
+
+    /**
+     * Create and send a report of operations for the current run
+     *
+     * @param results a list of [Result]s to check and report on
+     */
+    fun report(results: Sequence<Result>) {
+        val body = results.joinToString(
+                prefix = "Report for ${ZonedDateTime.now().toLocalDate()}\n",
+                separator = "\n------------------------------------------------------------\n",
+                transform = Result::message)
+
+        if (body.isNotBlank()) {
+            send(body)
+        }
+    }
+
+    private fun send(body: String) {
+        // push to property?
+        val smtpFrom = "chronopolis-mail@umiacs.umd.edu"
+
+        val properties = Properties()
+        properties["mail.smtp.host"] = "localhost.localdomain"
+        val session = Session.getDefaultInstance(properties)
+        val message = MimeMessage(session)
+        message.setFrom(smtpFrom)
+        message.setRecipients(Message.RecipientType.TO, smtpConfig.to)
+
+        message.subject = "Duracloud Bridge Restoration Report"
+        message.setContent(body, "text/plain")
+        Transport.send(message)
+    }
+
+}
